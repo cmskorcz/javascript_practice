925d2217748d38af4cbf0756964d29adde221e44
cmskorcz <chaseskorey@icloud.com>
2022-04-04 23:51:58 -0400

    initial commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+// Key-Value pair w/ method:
+
+// const cars = {
+//     make: 'Honda',
+//     model: 'Civic',
+//     year: 2003,
+//     displayList: function () {
+//         alert('Make: ' + cars.make);
+//     }
+// };
+
+// Method called:
+
+// cars.displayList();
+
+// Function:
+
+// const displayList = function () {
+//     alert('Make: ' + cars.make)
+// };
+
+// Function Call:
+
+// displayList();
